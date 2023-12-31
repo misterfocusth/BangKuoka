ff4fc4afece1a5ba1ac69476038d62ce13cb36d5
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-12-31 18:49:33 +0700

    feat: add google-font (Noto Sans)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { Inter } from "next/font/google";
 import "../globals.css";
 
 // Antd
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { ConfigProvider } from "antd";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -25,7 +22,7 @@ export default function RootLayout({
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
       </head>
-      <body className={inter.className} id="app">
+      <body id="app">
         <AntdRegistry>
           <ConfigProvider
             theme={{
