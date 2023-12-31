3efb42261bd0bdcd0dfe23e9ce84d014770cc67e
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2023-12-31 18:44:02 +0700

    feat: add antd for ui-library and page metadata

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,20 @@
+import { Button, ConfigProvider } from "antd";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
   const t = useTranslations("Index");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>{t("login_suggest_label")}</p>
-    </main>
+    <ConfigProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <p>{t("login_suggest_label")}</p>
+        <Button className="w-full font-bold" size="large" type="primary">
+          {t("login_button_title")}
+        </Button>
+        <Button className="w-full font-bold" size="large" type="default">
+          Cancel
+        </Button>
+      </main>
+    </ConfigProvider>
   );
 }
