cbe4d3ee7b1953a99745cb861367be61c8bbc868
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-01-03 19:55:45 +0700

    feat: support PWA for install on mobile devices

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+// Next-PWA
+const withPWA = require("next-pwa");
+
+// Next-Intl
 const withNextIntl = require("next-intl/plugin")('./src/i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    ...withPWA({
+        dest: 'public',
+        register: true,
+        skipWaiting: true
+    })
+}
 
 module.exports = withNextIntl(nextConfig)
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -20,6 +20,8 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#136912" />
       </head>
       <body id="app">
         <AntdRegistry>
