e3ebb869ad15a23b458fc27492522f0cf255cd0e
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2024-02-25 01:08:49 +0700

    feat: integrated api for show organizer's stats

--- src/app/[locale]/(Organizer)/organizer/credential/page.tsx
+++ src/app/[locale]/(Organizer)/organizer/credential/page.tsx
@@ -1,19 +1,53 @@
 "use client";
 
+import { db } from "@/app/config/firebaseConfig";
+import { Organizer } from "@/app/types/organizer";
+import { AuthContext } from "@/contexts/AuthContext";
 import { useRouter } from "@/navigation";
 import { Button } from "antd";
 import Password from "antd/es/input/Password";
+import { doc, updateDoc } from "firebase/firestore";
 import { KeyRoundIcon } from "lucide-react";
-import { FC, useState } from "react";
+import { FC, useContext, useState } from "react";
+import toast from "react-hot-toast";
 
 const CredentialPage = () => {
+  const authContext = useContext(AuthContext);
+  const currentUser = useContext(AuthContext).currentUser as Organizer;
   const router = useRouter();
 
   const [newPassword, setNewPassword] = useState("");
   const [currentPassword, setCurrentPassword] = useState("");
   const [cfNewPassword, setCfNewPassword] = useState("");
 
-  const handleChangePassword = () => {};
+  const handleChangePassword = async () => {
+    if (!currentPassword) {
+      toast.error("Error, current password is missing.");
+      return;
+    } else if (!(newPassword && cfNewPassword)) {
+      toast.error("Error, password field(s) are missing.");
+      return;
+    } else if (newPassword !== cfNewPassword) {
+      toast.error("Error, password mismatch!");
+      return;
+    }
+
+    if (currentPassword !== currentUser.password) {
+      toast.error("Error, current password incorrect!");
+      return;
+    }
+
+    if (confirm("Comfirm to change password, you might need to login again.")) {
+      updateDoc(doc(db, "organizers", currentUser.id), {
+        password: cfNewPassword,
+      })
+        .then(() => {
+          authContext.logout("ORGANIZER");
+          router.replace("/organizer");
+        })
+        .catch((err) => console.error(err));
+    }
+  };
 
   return (
     <div>
